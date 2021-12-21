5731ba68902dc5e9289b6fd7e85e570d9e218b13
anthony sanchez <anthonysa0813@gmail.com>
2021-12-21 10:36:38 -0500

    first commit

---
+++ data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    _id: "1",
+    name: "Limon verde",
+    image: "/images/limon.png",
+    price: 89.99,
+    discount: 10,
+  },
+  {
+    _id: "2",
+    name: "Napal Limpio",
+    image: "/images/napal.png",
+    price: 59.99,
+    discount: 15,
+  },
+  {
+    _id: "3",
+    name: "Palta fuerte",
+    image: "/images/palta.png",
+    price: 39.99,
+    discount: 5,
+  },
+  {
+    _id: "4",
+    name: "Tomate verde",
+    image: "/images/tomate-verde.png",
+    price: 89.99,
+    discount: 10,
+  },
+  {
+    _id: "5",
+    name: "Tomate rojo",
+    image: "/images/tomate.png",
+    price: 69.99,
+    discount: 10,
+  },
+  {
+    _id: "6",
+    name: "Pechuga de pavo",
+    image: "/images/pechuga-pavo.png",
+    price: 89.99,
+    discount: 18,
+  },
+  {
+    _id: "7",
+    name: "Huevos de granja",
+    image: "/images/huevos.png",
+    price: 29.99,
+    discount: 20,
+  },
+  {
+    _id: "8",
+    name: "Pack de pasta dental",
+    image: "/images/pasta-dental.png",
+    price: 49.99,
+    discount: 10,
+  },
+  {
+    _id: "9",
+    name: "Pan bimbo",
+    image: "/images/pan-bimbo.png",
+    price: 39.99,
+    discount: 30,
+  },
+  {
+    _id: "10",
+    name: "Mc Cormick Mayonesa",
+    image: "/images/maypnesa.png",
+    price: 19.99,
+    discount: 50,
+  },
+];
+
+module.exports = products;
